afd8b839ff964e7107d16005a59a805accb861df
gutclone <47323188+gutclone@users.noreply.github.com>
2019-03-12 08:37:47 +0100

    Tool-LX

---
+++ Tools-SF2.py
@@ -0,0 +1,76 @@
+import os
+print('\033[1;36m')
+print("*****************************************************")
+print("             • Hello $Salim Fouaissi$")
+print("*****************************************************")
+print("                   -telecharger:↓")
+print("                   --------------")
+
+print("_________________")
+print("                 |")
+print(" •1: sqlmap")
+print("                 |")
+print(" •2: Malicious")
+print("                 |")
+print(" •3: hammer")
+print("                 |")
+print(" •4: socialFish")
+print("                 |")
+print(" •5: payload5")
+print("                 |")
+print(" •6: payload6")
+print("                 |")
+print(" •7: Tools-X")
+print("                 |")
+print(" •8: kwetza")
+print("                 |")
+print(" •9: HashCode")
+print("                 |")
+print(" •10: termux-sudo")
+print("                 |")
+print("__________________")
+print("*****************************************************")
+
+print("           • SALIM: ✓")
+print("           ----------")
+y=str(input("               • Enter a number:"))
+
+print("-----------------------------------------------------")
+print("                     *install*")
+print("-----------------------------------------------------")
+
+if y=="1":
+	os.system("git clone https://github.com/sqlmapproject/sqlmap")
+
+elif y=="2":
+	os.system("git clone https://github.com/Hider5/Malicious")
+	
+elif y=="3":
+	os.system("git clone https://github.com/cyweb/hammer")
+	
+elif y=="4":
+	os.system("git clone https://github.com/UndeadSec/SocialFish.git")
+	
+elif y=="5":
+	os.system("git clone https://github.com/Ali898989/setupp")
+	
+elif y=="6":
+	os.system("git clone https://github.com/amerlaceset/Amer")
+
+elif y=="7":
+	os.system("git clone https://github.com/RajkumrdusadTool-X")
+	
+elif y=="8":
+	os.system("git clone https://github.com/sensepost/kwetza.git")
+	
+elif y=="9":
+	os.system("git clone https://github.com/Sup3r-Us3r/HashCode")
+	
+elif y=="10":
+	os.system("git clone https://github.com/st42/termux-sudo")
+	
+else:
+	print("errer")
+
+print("_____________________________________________________")
+	
